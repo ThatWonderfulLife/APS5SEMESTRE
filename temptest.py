6cce1d711670b8f26262c4b215f9a8ff13bcecea
WonderfulLife <61920829+ThatWonderfulLife@users.noreply.github.com>
2022-05-14 17:25:34 -0300

    Add files via upload

---
+++ temptest.py
@@ -0,0 +1,8 @@
+import wmi
+w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+temperature_infos = w.Sensor()
+for sensor in temperature_infos:
+    if sensor.SensorType==u'Temperature':
+
+        if sensor.Name == "CPU Core #1":
+            print(sensor.Value)
