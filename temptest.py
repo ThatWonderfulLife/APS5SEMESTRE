c8a03054de6763e5e4c89aaa42f834c07eda0302
WonderfulLife <61920829+ThatWonderfulLife@users.noreply.github.com>
2022-05-15 17:09:16 -0300

    Delete temptest.py

--- temptest.py
+++
@@ -1,8 +0,0 @@
-import wmi
-w = wmi.WMI(namespace="root\OpenHardwareMonitor")
-temperature_infos = w.Sensor()
-for sensor in temperature_infos:
-    if sensor.SensorType==u'Temperature':
-
-        if sensor.Name == "CPU Core #1":
-            print(sensor.Value)
